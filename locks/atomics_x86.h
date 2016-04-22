682682df25423d7f96f37f5b621e200550329985
miyengar <m-iyengar@github.com>
2016-04-21 20:11:51 -0400

    correct atomics names

--- locks/atomics_x86.h
+++ locks/atomics_x86.h
@@ -20,7 +20,7 @@ uint64_t _lock_cmpxchg_64(uint64_t *data, uint64_t old_val, uint64_t new_val);
  *	@param new_val The value to replace with if check succeeds
  *	@return The original value at the memory location
  */
-uint64_t _lock_cmpxchg_64(uint64_t *data, uint64_t new_val);
+uint64_t _lock_xchg_64(uint64_t *data, uint64_t new_val);
 
 /**	
  *	@brief atomic fetch-and-add wrapper for x86_64 
@@ -29,6 +29,6 @@ uint64_t _lock_cmpxchg_64(uint64_t *data, uint64_t new_val);
  *	@param addend The value to add to memory location
  *	@return The original value at the memory location
  */
-uint64_t _lock_cmpxchg_64(uint64_t *data, uint64_t addend);
+uint64_t _lock_xadd_64(uint64_t *data, uint64_t addend);
 
 #endif /* ATOMICS_X86_H */
