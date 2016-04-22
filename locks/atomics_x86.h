6489f556432b4f069822d0eb739680e6d81b12b6
miyengar <m-iyengar@github.com>
2016-04-21 20:08:08 -0400

    added x86_64 atomic instruction wrappers and headers

---
+++ locks/atomics_x86.h
@@ -0,0 +1,34 @@
+#ifndef ATOMICS_X86_H
+#define ATOMICS_X86_H
+
+#include <stddef.h>
+
+/**	
+ *	@brief atomic compare-and-swap wrapper for x86_64 
+ *
+ *	@param data Pointer to memory location
+ *	@param old_val The value to check against
+ *	@param new_val The value to replace with if check succeeds
+ *	@return The original value at the memory location
+ */
+uint64_t _lock_cmpxchg_64(uint64_t *data, uint64_t old_val, uint64_t new_val);
+
+/**	
+ *	@brief atomic swap wrapper for x86_64 
+ *
+ *	@param data Pointer to memory location
+ *	@param new_val The value to replace with if check succeeds
+ *	@return The original value at the memory location
+ */
+uint64_t _lock_cmpxchg_64(uint64_t *data, uint64_t new_val);
+
+/**	
+ *	@brief atomic fetch-and-add wrapper for x86_64 
+ *
+ *	@param data Pointer to memory location
+ *	@param addend The value to add to memory location
+ *	@return The original value at the memory location
+ */
+uint64_t _lock_cmpxchg_64(uint64_t *data, uint64_t addend);
+
+#endif /* ATOMICS_X86_H */
