4f7237cd798073ab6b23f7157dc57155f95547d8
miyengar <m-iyengar@github.com>
2016-05-02 23:07:22 -0500

    reverted to gcc intrinsics for atomics (avoid linking issues with icc), added alignment to structs to eliminate false sharing

--- locks/mcs_queue_rwlock.c
+++ locks/mcs_queue_rwlock.c
@@ -10,25 +10,29 @@ void mcs_rlock(mcs_rwlock_t *lock, rwlock_qnode_t *qnode) {
 	*qnode = INIT_RLOCK_QNODE;
 
 	// set self as next locker (and try to take lock)
-	rwlock_qnode_t *prev = lock_xchg_64(&lock->tail, qnode);
+	//rwlock_qnode_t *prev = lock_xchg_64(&lock->tail, qnode);
+	rwlock_qnode_t *prev = __sync_lock_test_and_set(&lock->tail, qnode);
 
 	// if no previous locker
 	if (!prev) {
-		lock_xadd_64(&lock->num_read, 1);
+		//lock_xadd_64(&lock->num_read, 1);
+		__sync_fetch_and_add(&lock->num_read, 1);
 		qnode->state = STATE_SET_SPIN(qnode->state, 0);
 	}
 	// there is a previous locker
 	else {
 		if ((prev->type == RWLOCK_WRITE) ||
-				(lock_cmpxchg_64(&prev->state, STATE_WAIT_NIL, STATE_WAIT_READ) 
-					== STATE_WAIT_NIL)) {
+				//(lock_cmpxchg_64(&prev->state, STATE_WAIT_NIL, STATE_WAIT_READ) 
+					//== STATE_WAIT_NIL)) {
+				__sync_bool_compare_and_swap(&prev->state, STATE_WAIT_NIL, STATE_WAIT_READ)) {
 			// if predecessor is a writer || is waiting reader, wait to be released
 			prev->next = qnode;
 			while (STATE_GET_SPIN(qnode->state));
 		}
 		else {
 			// otherwise, got read lock, increment count
-			lock_xadd_64(&lock->num_read, 1);
+			//lock_xadd_64(&lock->num_read, 1);
+			__sync_fetch_and_add(&lock->num_read, 1);
 			prev->next = qnode;
 			qnode->state = STATE_SET_SPIN(qnode->state, 0);
 		}
@@ -37,14 +41,16 @@ void mcs_rlock(mcs_rwlock_t *lock, rwlock_qnode_t *qnode) {
 	// check for next thread, if reader, allow to proceed
 	if (STATE_GET_NEXT(qnode->state) == RWLOCK_READ) {
 		while (!qnode->next);
-		lock_xadd_64(&lock->num_read, 1);
+		//lock_xadd_64(&lock->num_read, 1);
+		__sync_fetch_and_add(&lock->num_read, 1);
 		qnode->next->state = STATE_SET_SPIN(qnode->state, 0);
 	}
 }
 
 void mcs_runlock(mcs_rwlock_t *lock, rwlock_qnode_t *qnode) {
 	// check for next thread
-	if (qnode->next || (lock_cmpxchg_64(&lock->tail, qnode, NULL) == qnode)) {
+	if (qnode->next || //(lock_cmpxchg_64(&lock->tail, qnode, NULL) == qnode)) {
+			__sync_bool_compare_and_swap(&lock->tail, qnode, NULL)) {
 		// wait for next thread to identify itself
 		while (!qnode->next);
 		// if next is writer, set it as the 
@@ -54,9 +60,11 @@ void mcs_runlock(mcs_rwlock_t *lock, rwlock_qnode_t *qnode) {
 
 	// if last reader, wake any writer if it exists
 	rwlock_qnode_t *writer;
-	if ((lock_xadd_64(&lock->num_read, -1) == 1) && 
+	//if ((lock_xadd_64(&lock->num_read, -1) == 1) && 
+	if ((__sync_fetch_and_add(&lock->num_read, -1) == 1) && 
 			(writer = lock->next_wr) && !lock->num_read && 
-			(lock_cmpxchg_64(&lock->next_wr, writer, NULL) == writer))
+			//(lock_cmpxchg_64(&lock->next_wr, writer, NULL) == writer))
+			__sync_bool_compare_and_swap(&lock->next_wr, writer, NULL))
 		writer->state = STATE_SET_SPIN(writer->state, 0);
 }
 
@@ -64,7 +72,8 @@ void mcs_wlock(mcs_rwlock_t *lock, rwlock_qnode_t *qnode) {
 	*qnode = INIT_WLOCK_QNODE;
 
 	// get previous thread (and try to set as owner)
-	rwlock_qnode_t *prev = lock_xchg_64(&lock->tail, qnode);
+	//rwlock_qnode_t *prev = lock_xchg_64(&lock->tail, qnode);
+	rwlock_qnode_t *prev = __sync_lock_test_and_set(&lock->tail, qnode);
 
 	// check prev predecessor
 	if (prev) 
@@ -73,7 +82,8 @@ void mcs_wlock(mcs_rwlock_t *lock, rwlock_qnode_t *qnode) {
 	else {
 		// otherwise, this is the next writer, wait for all readers to leave
 		lock->next_wr = qnode;
-		if (!lock->num_read && (lock_xchg_64(&lock->next_wr, NULL) == qnode))
+		if (!lock->num_read && //(lock_xchg_64(&lock->next_wr, NULL) == qnode))
+				(__sync_lock_test_and_set(&lock->next_wr, NULL) == qnode))
 			qnode->state = STATE_SET_SPIN(qnode->state, 0);
 	}
 
@@ -83,12 +93,14 @@ void mcs_wlock(mcs_rwlock_t *lock, rwlock_qnode_t *qnode) {
 
 void mcs_wunlock(mcs_rwlock_t *lock, rwlock_qnode_t *qnode) {
 	// check (and wait) for successor
-	if (qnode->next || !(lock_cmpxchg_64(&lock->tail, qnode, NULL) == qnode)) {
+	if (qnode->next || //!(lock_cmpxchg_64(&lock->tail, qnode, NULL) == qnode)) {
+			!__sync_bool_compare_and_swap(&lock->tail, qnode, NULL)) {
 		while (qnode->next);
 
 		// increment read count if next is a reader
 		if (qnode->next->type == RWLOCK_READ)
-			lock_xadd_64(&lock->num_read, 1);
+			//lock_xadd_64(&lock->num_read, 1);
+			__sync_fetch_and_add(&lock->num_read, 1);
 	
 		// acknowledge next thread
 		qnode->next->state = STATE_SET_SPIN(qnode->next->state, 0);
--- locks/mcs_ticket_rwlock.c
+++ locks/mcs_ticket_rwlock.c
@@ -8,7 +8,8 @@
 
 void ticket_rlock(ticket_rwlock_t *lock) {
 	// get ticket and increment read count
-	uint64_t prev_writers = lock_xadd_64(&lock->requests, RCNT_INC) & W_MASK;
+	//uint64_t prev_writers = lock_xadd_64(&lock->requests, RCNT_INC) & W_MASK;
+	uint64_t prev_writers = __sync_fetch_and_add(&lock->requests, RCNT_INC) & W_MASK;
 
 	// spin until the write completions match the ticket
 	int wait = 0, cnt;
@@ -26,7 +27,8 @@ void ticket_runlock(ticket_rwlock_t *lock) {
 
 void ticket_wlock(ticket_rwlock_t *lock) {
 	// get ticket and increment write count
-	uint64_t prev_threads = lock_xadd_64(&lock->requests, WCNT_INC);
+	//uint64_t prev_threads = lock_xadd_64(&lock->requests, WCNT_INC);
+	uint64_t prev_threads = __sync_fetch_and_add(&lock->requests, WCNT_INC);
 
 	// spin until the request completion matches ticket
 	int wait = 0, cnt;
